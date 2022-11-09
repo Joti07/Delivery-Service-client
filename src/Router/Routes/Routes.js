ab376e18819812eb4a7f3c008bab956657cd83d6
Joti <tanjilajoti@gmail.com>
2022-11-09 16:49:11 +0600

    add checkout page

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,5 +1,6 @@
 import Main from "../../Layout/Main/Main";
 import Blogs from "../../Pages/Blogs/Blogs";
+import Checkout from "../../Pages/Checkout/Checkout";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
@@ -27,6 +28,15 @@ const router = createBrowserRouter([
                 path: '/blogs',
                 element: <Blogs></Blogs>
             },
+            {
+                path: '/checkout/:id',
+                element: <Checkout></Checkout>,
+                loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
+            },
+            {
+                path: '/orders',
+                //   element: <Orders></Orders>
+            }
         ]
     }
 ]);
